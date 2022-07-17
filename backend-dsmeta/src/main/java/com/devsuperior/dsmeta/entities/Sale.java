7c1e78ea280113ffc79136b130f6a51410eb4801
Mogleson Lima <moglesonlima@alu.ufc.br>
2022-07-17 15:10:56 -0300

    chore(backend-dsmeta/*/resources): Adicionando config. e dados refetentes ao banco.

---
+++ backend-dsmeta/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+//Lombok
+@Getter
+@Setter
+//persistence
+@Entity
+@Table(name="tb_sales")
+public class Sale {
+	//Identificando ID
+	@Id
+	//definindo como será esse id(autoincrement)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	public Sale() {
+		
+	}
+
+}
