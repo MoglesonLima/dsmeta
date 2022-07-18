49d5163645f6b04f3f5974f4fe6cf0f2d4a306bf
Mogleson Lima <moglesonlima@alu.ufc.br>
2022-07-18 00:22:38 -0300

    chore(frontend-dsmeta/): Adicionado ação de notificar via sms via onClick do notification btn.

--- frontend-dsmeta/src/components/SalesCard/index.tsx
+++ frontend-dsmeta/src/components/SalesCard/index.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
 import NotificationButton from '../NoticationButton'
+import { BASE_URL } from "../utils/request";
 import './styles.css'
 
 function SalesCard() {
@@ -9,10 +12,29 @@ function SalesCard() {
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
     const max = new Date()
 
-
     //Iniciando o uso de React Hook
     const [minDate, setMinDate] = useState(min)
     const [maxDate, setMaxDate] = useState(max)
+
+    const [sales, setSales] = useState<Sale[]>([])
+
+    useEffect(() => {
+        //formatacao data para request(slice pegar apenas posições 0 até 10)
+        const dmin = minDate.toISOString().slice(0, 10)
+        const dmax = maxDate.toISOString().slice(0, 10)
+
+
+        console.log(dmin)
+
+        //BASE_URL indica onde será efetuada a requisicao
+        axios.get(`${BASE_URL}sales?minDate=${dmin}&maxDate=${dmax}`)
+            .then((response) => {
+                setSales(response.data.content)
+            })
+            .catch((erroHttp) => {
+                console.log("Erro -> " + erroHttp)
+            })
+    }, [minDate, maxDate])
 
     return (
         <div className="dsmeta-card">
@@ -40,6 +62,7 @@ function SalesCard() {
 
             <div>
                 <table className="dsmeta-sales-table">
+
                     <thead>
                         <tr>
                             <th className="show992">ID</th>
@@ -51,40 +74,23 @@ function SalesCard() {
                             <th>Notificar</th>
                         </tr>
                     </thead>
+
                     <tbody>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <NotificationButton />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <NotificationButton />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <NotificationButton />
-                            </td>
-                        </tr>
+                        {sales.map(sale => {
+                            return (
+                                <tr key={sale.id}>
+                                    <td className="show992">{sale.id}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="show992">{sale.visited}</td>
+                                    <td className="show992">{sale.deals}</td>
+                                    <td>R$ {sale.amount.toFixed(2)}</td>
+                                    <td>
+                                        <NotificationButton saleId={sale.id}/>
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
 
                 </table>
---
+++ frontend-dsmeta/src/components/utils/request.ts
@@ -0,0 +1,2 @@
+//Identificando de onde está vindo a base de dados
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-mogly.herokuapp.com/";
